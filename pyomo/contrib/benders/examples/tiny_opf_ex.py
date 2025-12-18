92589c0f1f5cbbbc72d19ad297487eb80739b240
Matthew Viens <75225878+viens-code@users.noreply.github.com>
2025-12-17 20:05:27 -0600

    Updating tiny_opf_ex still getting weird results

--- pyomo/contrib/benders/examples/tiny_opf_ex.py
+++ pyomo/contrib/benders/examples/tiny_opf_ex.py
@@ -14,9 +14,20 @@ import pyomo.environ as pyo
 
 import itertools
 from math import pi as pi_value
+import pprint
 
 """
 EXAMPLE IN DEVELOPMENT
+To run this example:
+
+mpirun -np X python tiny_opf_ex.py
+Where X is the number of processes, suggest using 2-3
+
+If altering this script, note the more robust error handling available by using:
+mpirun -np X python -m mpi4py tiny_opf_ex.py
+Where X is again the number of processes.
+This standardizes error handling especially for unexpected and unhandled errors
+
 DC-Optimal Power Flow Example.
 This is a feasibility-only Benders Decomposition example.
 The second-stage contributes only to feasibility cuts.
@@ -30,22 +41,53 @@ https://arxiv.org/abs/2511.08805
 class Grid:
     def __init__(self):
         # model data
-        load_dict = {"bus1": 0, "bus2": 0, "bus3": 100}
-        cost_dict = {"bus1": 50, "bus2": 50, "bus3": 0}
-        susceptance_dict = {
+
+        #we can change these three parameters to get a unique value
+        #originally not designed to be unique
+
+        # TODO: revisit these values to make a non-trivial feasibility cut
+        # will likely need to change flow limits
+        #system solves with this data (always feasible)
+
+        # original values
+        self.load_dict = {"bus1": 0, "bus2": 0, "bus3": 100}
+        self.gen_max_dict = {"bus1": 100, "bus2": 100, "bus3": 0}
+        self.cost_dict = {"bus1": 50, "bus2": 50, "bus3": 0}
+        self.flow_bounds_dict = {
             ("bus1", "bus2"): 100,
             ("bus1", "bus3"): 100,
             ("bus2", "bus3"): 100,
         }
-        gen_max_dict = {"bus1": 100, "bus2": 100, "bus3": 0}
-        flow_bounds_dict = {
+
+        #fails with this data, feasible set gen_bus2 = 100, flow_2_to_3 = 100
+        #TODO: confirm with the nondecomposed script that this solves with this data
+        # self.load_dict = {"bus1": 0, "bus2": 0, "bus3": 100}
+        # self.gen_max_dict = {"bus1": 50, "bus2": 100, "bus3": 0}
+        # self.cost_dict = {"bus1": 50, "bus2": 50, "bus3": 0}
+        # self.flow_bounds_dict = {
+        #     ("bus1", "bus2"): 0,
+        #     ("bus1", "bus3"): 0,
+        #     ("bus2", "bus3"): 100,
+        # }
+
+        #rest of data
+        
+        self.susceptance_dict = {
             ("bus1", "bus2"): 100,
             ("bus1", "bus3"): 100,
             ("bus2", "bus3"): 100,
         }
-        theta_bounds_dict = {"bus1": pi_value, "bus2": pi_value, "bus3": pi_value}
-        buses = ["bus1", "bus2", "bus3"]
-        lines = itertools.combinations(buses, 2)
+        
+        self.flow_bounds_dict = {
+            #("bus1", "bus2"): 100,
+            ("bus1", "bus2"): 0,
+            #("bus1", "bus3"): 100,
+            ("bus1", "bus3"): 0,
+            ("bus2", "bus3"): 100,
+        }
+        self.theta_bounds_dict = {"bus1": pi_value, "bus2": pi_value, "bus3": pi_value}
+        self.buses = ["bus1", "bus2", "bus3"]
+        self.lines = itertools.combinations(self.buses, 2)
         # {('bus1', 'bus2'), ('bus1', 'bus3'), ('bus2', 'bus3')}
 
 
@@ -55,6 +97,9 @@ class Grid:
 # mode 1 is Network Flow
 # mode 2 is DC-OPF with flow variables
 def create_tiny_opf(grid, mode=2):
+    # print(f"{grid=}")
+    # print(f"{vars(grid)=}")
+    # print(f"{grid.buses=}")
     assert mode in [0, 1, 2], 'Needs to be mode 0, 1, or 2'
     if mode == 0:
         print('Model in Copper Plate Mode')
@@ -100,14 +145,14 @@ def create_tiny_opf(grid, mode=2):
                 if i == node:
                     yield j
 
-        model.NodesOut = pyo.Set(model.buses, initialize=grid.NodesOut_init)
+        model.NodesOut = pyo.Set(model.buses, initialize=NodesOut_init)
 
         def NodesIn_init(m, node):
             for i, j in m.lines:
                 if j == node:
                     yield i
 
-        model.NodesIn = pyo.Set(model.buses, initialize=grid.NodesIn_init)
+        model.NodesIn = pyo.Set(model.buses, initialize=NodesIn_init)
 
         def flow_max_rule(m, i, j):
             return (-m.flow_max[i, j], m.flow_max[i, j])
@@ -149,27 +194,31 @@ def create_tiny_opf(grid, mode=2):
 def create_root(grid):
     # If first-stage is generation and second-stage is flow and angles
     # the first-stage model is just copper-plate
-    m = create_tiny_opf(grid, mode=0)
-    # add unbounded var as eta placeholder
-    m.eta = pyo.Var()
+    #print('Creating Root')
+    m = create_tiny_opf(grid=grid, mode=0)
+    # add unbounded var as eta placeholder since this is feasibility only
+    # need to initialize to trivial value to avoid None issues
+    m.eta = pyo.Var(initialize = 0.0)
+    #print('Done Creating Root')
     return m
 
 
 def create_subproblem(root, grid):
     # the second-stage model is full DC-OPF
-    m = create_tiny_opf(grid, mode=2)
+    #print('Creating Subproblem')
+    m = create_tiny_opf(grid = grid, mode=2)
 
     complicating_vars_map = pyo.ComponentMap()
     for b in grid.buses:
         complicating_vars_map[root.generation[b]] = m.generation[b]
-
+    #print('Done creating subproblem')
     return m, complicating_vars_map
 
 
 def main():
     t0 = time.time()
     grid = Grid()
-    m = create_root(grid=Grid)
+    m = create_root(grid=grid)
     root_vars = list(m.generation.values())
     m.benders = BendersCutGenerator()
     m.benders.set_input(root_vars=root_vars, tol=1e-8)
@@ -189,6 +238,10 @@ def main():
     )
     for i in range(30):
         res = opt.solve(m, tee=False)
+        # if i == 0:
+        #     print(f"Solver Status: {str(res.solver.status)}")
+        #     print(f"Termination Condition: {str(res.solver.termination_condition)}")
+        #     m.pprint()
         cuts_added = m.benders.generate_cut()
         for c in cuts_added:
             opt.add_constraint(c)
